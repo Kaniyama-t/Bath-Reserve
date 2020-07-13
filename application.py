f12bd7255ab4cc54fe8dd39e5cbb28441aa0b6a6
toshiyuki@home <haruyuki.program@gmail.com>
2020-07-13 14:08:49 +0900

    Deploy ver.2

--- application.py
+++ application.py
@@ -163,4 +163,4 @@ def user_resister():
     return render_template("user_regist_success.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
+    app.run(debug=False, host="0.0.0.0", port=5000, threaded=True)
