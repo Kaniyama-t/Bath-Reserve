a0f93cd7477bd0c22482cab623ecc424755c01b8
Kaniyama-t <e17266@kagawa.kosen-ac.jp>
2020-07-16 19:16:35 +0900

    [fix] (重要)/reserveへGETアクセスした際に400になる現象の修正・リファクタリング・(最重要)sessionオブジェクトのjavascriptからの操作不可の設定

--- application.py
+++ application.py
@@ -11,6 +11,13 @@ import numpy as np
 app = Flask(__name__)
 app.secret_key = str(random.randrange(9999999999999999))
 
+# session['*']が外部から操作されないよう対策
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
+
 # 入浴時間リスト
 times = []
 times.append(datetime.datetime.strptime('17:00', "%H:%M"))
@@ -21,18 +28,70 @@ while i < len(times):
     times[i] = times[i].strftime("%H:%M")
     i += 1
 
+# --------------------------------
+# モバイル版仕様
+# １．初回アクセス
+# 　古いバージョンのUIが表示されます
+# 
+# ２．新・旧UI表示の仕組み
+# 　すべてのmake_responce関数において，sessionの"TemplateRootPath"に指定されたディレクトリ直下のテンプレートを読み込むようにする．
+# 
+# ３．新・旧UI切替の仕組み
+# 　POSTかGETでui=ver2を入れると，before_request関数でsessionの値が書き換わり新しいバージョンに切り替わる．
+# --------------------------------
+# モバイル版対応コードここから
+# --------------------------------
+
+# POSTかGETのすべてのリクエストが処理される前に実行
+def before_request():
+    # *******************************************************
+    # Login
+    if ('login_flag' not in session):
+        session['login_flag'] = False
+    # *******************************************************
+    # UI切替トリガ
+    # --- 初回アクセス --------------------------------------
+    if ('TemplateRootPath' not in session):
+        SwitchUI = 'default'
+    # --- 変更指定があったら(GET) ---------------------------
+    elif (request.method == 'GET') and (request.args.get('ui',default=None)):
+        SwitchUI = request.args.get('ui')
+        print(SwitchUI)
+    # --- 変更指定があったら(POST) --------------------------
+    elif (request.method == 'POST') and ('ui' in request.form):
+        SwitchUI = request.form['ui']
+    # --- 変更なし ------------------------------------------
+    else:
+        SwitchUI = None
+
+    # *******************************************************
+    # UI切替
+    # --- 新バージョン --------------------------------------
+    if SwitchUI == 'ver2':
+        session['TemplateRootPath'] = 'mobile/'
+    # elif modeq == 'ver3':
+    #     pass
+    # ...
+    # --- デフォルト ----------------------------------------
+    elif SwitchUI:
+        session['TemplateRootPath'] = ''
+    # --- 変更なし ------------------------------------------
+    else:
+        pass
+
+app.before_request(before_request)
+# --------------------------------
+# モバイル版対応コードここまで
+# --------------------------------
+
 # ログインページへの遷移
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    return render_template("mobile/index.html", Error=0)
+    return render_template(session['TemplateRootPath'] + "index.html", Error=0)
 
 # 予約入力画面への遷移
 @app.route("/reserve", methods=["GET", "POST"])
 def login_manager():
-    # ユーザ変数取得
-    print(request)
-    userid = request.form["userid"]
-    userpassword = request.form["password"]
     # ----------------------------------------------------------
     # DB接続
     # 
@@ -40,20 +99,49 @@ def login_manager():
     cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password)
     cursor = cnxn.cursor()
 
-    # DBからパスワード取得
-    sql = "SELECT userpassword FROM users WHERE userid = ?"
-    cursor.execute(sql, userid)
-    result = cursor.fetchone()
-    # パスワード認証
-    i = 0
-    while i<10:
-        userpassword = hashlib.sha256(userpassword.encode()).hexdigest()
-        i += 1
-    if userid is "" or userpassword is "":
-        return render_template("mobile/index.html", Error=1)
-    elif result[0] != userpassword:
-        return render_template("mobile/index.html", Error=2)
+    # ----------------------------------------------------------
+    # ログイン処理
+    # 
+    # ----------------------------------------------------------1433
+    # ※ログイン済みの場合はこの処理を飛ばして予約状況取得へ行きます．
+    # bug fixed     : /reserve決め打ちしてアクセスした時に400エラー発生を修正
+    # security fixed: パス登録をしていなくてもログインできる点を防止 
+    if (not session['login_flag']):
+        # ちゃんとuseridとpasswordに値が入っていて，未登録ユーザじゃないなら
+        if ('userid' in request.form) and ('password' in request.form) and (request.form["userid"]) and (request.form["password"]):
+            
+            userid = request.form["userid"]
+            userpassword = request.form["password"]
+
+            sql = "SELECT userpassword FROM users WHERE userid = ?"
+            cursor.execute(sql, userid)
+            result = cursor.fetchone()
+            # パスワード認証
+            i = 0
+            while i<10:
+                userpassword = hashlib.sha256(userpassword.encode()).hexdigest()
+                i += 1
+            
+            # パスワードによる認証結果の分岐
+            if result[0] == userpassword:
+                session['userid'] = userid
+                session['login_flag'] = True
+            else:
+                session['userid'] = ''
+                session['login_flag'] = False
+                return render_template(session['TemplateRootPath'] + "index.html", Error=2)
+            
+
+        # お前...さてはログイン情報入れずにリクエストしたやろ...
+        else:
+            return render_template(session['TemplateRootPath'] + "index.html", Error=1)
+    else:
+        userid = session['userid']
     
+    # ----------------------------------------------------------
+    # DBから時間帯あたりの予約者数を取得
+    # 
+    # ----------------------------------------------------------
     # initialize variable
     now = datetime.datetime.now()
     today = now.strftime("%Y_%m_%d ")
@@ -62,14 +150,18 @@ def login_manager():
     while i < len(times)-1:
         sql = sql + "SUM(CASE WHEN date = '" + today + times[i] + "' THEN 1 ELSE 0 END), "
         i += 1
-    sql_small = sql[:-2] +" FROM reserve WHERE bath_type = 0"
-    sql_large = sql[:-2] +" FROM reserve WHERE bath_type = 1"
+    sql_small = sql[:-2] +" FROM reserve WHERE bath_type = 0;"
+    sql_large = sql[:-2] +" FROM reserve WHERE bath_type = 1;"
     cursor.execute(sql_small)
     reservation_small = cursor.fetchone()
     cursor.execute(sql_large)
     reservation_large = cursor.fetchone()
+
+    # ----------------------------------------------------------
     # 既存の自身の予約を確認
-    sql = "SELECT bath_type, date FROM reserve WHERE userid=? AND date LIKE ?"
+    # 
+    # ----------------------------------------------------------
+    sql = "SELECT bath_type, date FROM reserve WHERE userid=? AND date LIKE ?;"
     cursor.execute(sql, userid, today+"%")
     
     result = cursor.fetchone()
@@ -88,16 +180,14 @@ def login_manager():
     cnxn.close()
 
     # ----------------------------------------------------------
-    # Generate Responce
+    # ページ・レスポンスの作成
     # 
     # ----------------------------------------------------------
-    session['userid'] = userid
-    session['login_flag'] = True
     session['reserved'] = reserved
     # dic = {'userid':userid, 'login_flag':True, 'reserved':reserved}
     # responce.set_cookie('cookie', value = json.dumps(dic))
     return make_response(
-        render_template("mobile/reserve.html",
+        render_template(session['TemplateRootPath'] + "reserve.html",
             today=now.strftime("%m/%d") ,
             userid=userid,
             times=times,
@@ -156,12 +246,12 @@ def reserve_register():
     session.pop('userid', None)
     session.pop('login_flag', None)
     session.pop('reserved', None)
-    return render_template("mobile/reserve_success.html", desired_time=desired_time, userid=userid)
+    return render_template(session['TemplateRootPath'] + "reserve_success.html", desired_time=desired_time, userid=userid)
     
 # ユーザー登録への遷移
 @app.route("/user_regist_form", methods=["GET", "POST"])
 def user_regist_form():
-    return render_template("mobile/user_regist_form.html")
+    return render_template(session['TemplateRootPath'] + "user_regist_form.html")
 
 # ユーザー登録の実行，登録完了画面への遷移
 @app.route("/user_register", methods=["GET","POST"])
@@ -183,7 +273,7 @@ def user_resister():
     cursor.execute(sql, userid)
     result = cursor.fetchone()
     if(result[0] != ""):
-        return render_template("mobile/user_regist_form.html", Error=2)
+        return render_template(session['TemplateRootPath'] + "user_regist_form.html", Error=2)
     # パスワードストレッチング
     i = 0
     while i<10:
@@ -197,7 +287,7 @@ def user_resister():
     cursor.close()
     cnxn.close()
     # 返却処理
-    return render_template("mobile/user_regist_success.html")
+    return render_template(session['TemplateRootPath'] + "user_regist_success.html")
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000, threaded=True)
+    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
